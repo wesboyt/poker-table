b0ac1a2d6fd992ff8716b59cef5c6c5dd7f2ce5e
wesboyt <30701972+wesboyt@users.noreply.github.com>
2025-10-16 17:20:40 -0700

    Update hand.py

--- hand.py
+++ hand.py
@@ -11,14 +11,15 @@ def parseAction(input):
     return playerSeat, playerChips
 
 class Hand:
-    def __init__(self, u_hand=None):
+    def __init__(self, u_hand=None, auto_deal=True):
+        self.auto_deal = auto_deal
         self.done = False
         if not u_hand:
-
-            self.u_hand = self._from_scratch(6,100,200,200 * 20 , random.randint(200 * 100, 200 * 500))
+            self.u_hand = self._from_scratch(6,100,200, 200 * 20, random.randint(200*100, 200 * 500))
+            self.active_players = 6
 
         else:
-            self.u_hand = [[], [], [], [], [], [], [], [], []]
+            self.u_hand = [[], [], [], [], [], [], [], []]
             for index, val in enumerate(u_hand):
                 sub_array = self.u_hand[index]
                 for sub_val in val:
@@ -28,6 +29,7 @@ class Hand:
             self.small_blind = parseAction(u_hand[2][-2])[1]
             stacks = []
             self.player_count = len(u_hand[2]) - 2
+            self.active_players = self.player_count
             for i in range(self.player_count):
                 stacks.append(parseAction(u_hand[2][i])[1])
             stacks = tuple(stacks)
@@ -59,18 +61,24 @@ class Hand:
             for action in u_hand[3]:
                 player, chips = parseAction(action)
                 actor = self.state.turn_index
-                invested = self.state.bets[actor]
+                if actor != None:
+                    invested = self.state.bets[actor]
+                else:
+                    invested = 0
 
 
                 if chips < 0:
-                    self.state.fold()
+                    if self.state.can_fold():
+                        self.state.fold()
+                    else:
+                        print("chips:", chips, "action:", action, "hand:", self.u_hand)
                 else:
                     minimum = self.state.checking_or_calling_amount
                     if chips <= minimum:
                         self.state.check_or_call()
                     else:
                         self.state.complete_bet_or_raise_to(chips + invested)
-            if street == 3:
+            if street > 0:
                 self.state.burn_card('??')
                 for i in range(3):
                     self.state.deal_board(u_hand[1][i])
@@ -86,7 +94,7 @@ class Hand:
                             self.state.check_or_call()
                         else:
                             self.state.complete_bet_or_raise_to(chips + invested)
-            elif street == 4:
+            if street > 3:
                 self.state.burn_card('??')
                 self.state.deal_board(u_hand[1][3])
                 for action in u_hand[5]:
@@ -101,7 +109,7 @@ class Hand:
                             self.state.check_or_call()
                         else:
                             self.state.complete_bet_or_raise_to(chips + invested)
-            elif street == 5:
+            if street > 4:
                 self.state.burn_card('??')
                 self.state.deal_board(u_hand[1][4])
                 for action in u_hand[6]:
@@ -121,22 +129,23 @@ class Hand:
         if self.done:
             return False
         #return player index of whos turn it is
-        #return available options fold, check, call, raise, unavailable options will be missing.
-        #0,{fold:0, check:0, call_chips:x, min_chips:x, max_chips:x, player:x}
+        #return available options fold, check, call, raise, allin, unavailable options will be missing.
+        #0,{fold:0, check:0, call:x, raise:(min, max), allin:x}
         #if it is time to deal cards, do so and return next player up's action space
+        #if it is end game
         options = {}
         if self.state.can_check_or_call():
             call_or_check = self.state.checking_or_calling_amount
             if self.state.checking_or_calling_amount == 0:
                 options['check'] = 0
             else:
-                options['call_chips'] = call_or_check
+                options['call'] = call_or_check
         if self.state.can_fold():
             options['fold'] = 0
         if self.state.min_completion_betting_or_raising_to_amount != None:
-            options['min_chips'] = self.state.min_completion_betting_or_raising_to_amount - self.state.bets[self.state.turn_index]
+            options['min_bet'] = self.state.min_completion_betting_or_raising_to_amount - self.state.bets[self.state.turn_index]
         if self.state.max_completion_betting_or_raising_to_amount != None:
-            options['max_chips'] = self.state.max_completion_betting_or_raising_to_amount - self.state.bets[self.state.turn_index]
+            options['max_bet'] = self.state.max_completion_betting_or_raising_to_amount - self.state.bets[self.state.turn_index]
         options['player'] = self.state.turn_index
         return options
 
@@ -153,17 +162,15 @@ class Hand:
             elif self.state.can_fold():
                 self.u_hand[self.state.street_index + 3].append("p" + str(current_player) + "f")
                 self.state.fold()
+                self.active_players = self.active_players - 1
             else:
                 print("ERROR: 0 chips when not possible.")
                 return False
         else:
             min = self.state.checking_or_calling_amount
-            if chips == min:
+            if chips <= min:
                 self.u_hand[self.state.street_index + 3].append("p" + str(current_player) + "c" + str(chips))
                 self.state.check_or_call()
-            elif chips < min:
-                print("ERROR:", chips, min, self.state.bets)
-                return False
             else:
                 if self.state.min_completion_betting_or_raising_to_amount == None:
                     size = self.state.checking_or_calling_amount
@@ -183,13 +190,15 @@ class Hand:
                     self.state.complete_bet_or_raise_to(chips + self.state.bets[self.state.turn_index])
         if self.state.can_select_runout_count():
             self.state.select_runout_count(1)
-        while self.state.can_burn_card():
-            self.state.burn_card('??')
-            while self.state.can_deal_board():
-                board_cards = map(self._state_card_to_text, self.state.deal_board().cards)
-                for card in board_cards:
-                    self.u_hand[1].append(card)
-        if self.state.street == None:
+        if self.active_players > 1:
+            if self.auto_deal:
+                while self.state.can_burn_card():
+                    self.state.burn_card('??')
+                    while self.state.can_deal_board():
+                        board_cards = map(self._state_card_to_text, self.state.deal_board().cards)
+                        for card in board_cards:
+                            self.u_hand[1].append(card)
+        if self.state.street == None and self.auto_deal:
             for player_index, payoff in enumerate(self.state.payoffs):
                 if payoff > 0:
                     self.u_hand[-1].append("p" + str(player_index) + "c" + str(payoff))
@@ -214,7 +223,7 @@ class Hand:
         self.small_blind = small_blind
         stacks = []
         self.player_count = player_count
-        self.u_hand = [[], [], [], [], [], [], [], [], []]
+        self.u_hand = [[], [], [], [], [], [], [], []]
 
         for i in range(player_count):
             stack = random.randint(min_stack_size, max_stack_size)
@@ -241,17 +250,11 @@ class Hand:
             stacks,
             self.player_count,
         )
-        #self.state.mode = Mode.CASH_GAME
         self.preflop_stacks = self.state.starting_stacks
         for i in range(self.player_count):
             hole_cards = "".join(map(self._state_card_to_text,self.state.deal_hole(2).cards))
             self.u_hand[0].append(hole_cards)
         return self.u_hand
-
-#self.state.hole_cards
-#self.state.payoffs->win/loss
-#self.state.board_cards
-#self.state.turn_index()
 
 if __name__ == '__main__':
     hand = Hand([["","","","","","9hKc"],["Js","Ts","9s"],["p0c100","p1c99","p2c109","p3c102","p4c110","p5c85","p0c1","p1c2"],["p2f","p3f","p4f","p5c43","p0f", "p1c41"],[],[],[]])
